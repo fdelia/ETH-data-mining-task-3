1ed0544bc17a4c3cbca89f88e954dd8ff2ddd5a4
fdelia <fabio_d@gmx.net>
2017-12-01 18:32:43 +0100

    1

--- example.py
+++ example.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+
 def mapper(key, value):
     # key: None
     # value: one line of input file
+    # value: 2D numpy array
+
     yield "key", "value"  # this is how you yield a key, value pair
 
 
@@ -10,4 +13,8 @@ def reducer(key, values):
     # key: key from mapper used to aggregate
     # values: list of all value for that key
     # Note that we do *not* output a (key, value) pair here.
+
+    # Output: 200 vectors representing the selected centers
+    #   each being 250 floats
+
     yield np.random.randn(200, 250)
