a2026debde5e9cd3463406463d066d93d0c9c532
fdelia <fabio_d@gmx.net>
2017-12-03 16:06:05 +0100

    1

--- kmeanspp.py
+++ kmeanspp.py
@@ -25,19 +25,22 @@ def mapper(key, value):
     distances = np.zeros(len(value))
     for v, vector in enumerate(value):
         distances[v] = measureDistance(vector, centers[0])
+    distances = np.square(distances)
     distances_norm = distances / np.sum(distances)
 
     # Choose the next mean µ randomly from the set X, where the probability
     # of a point x ∈ X being chosen is proportional to D(x), and add µ to M.
     while len(centers) < k:
         z = np.random.choice(np.arange(0, len(value)), p=distances_norm)
+        # z = np.argmax(distances_norm)
         centers.append(value[z])
 
         distances_all = np.zeros((len(centers), len(value)))
         for c, center in enumerate(centers):
             for v, vector in enumerate(value):
                 distances_all[c][v] = measureDistance(center, vector)
-        distances = np.amin(distances_all, axis=0)
+        distances = np.amin(distances_all, axis=0)  # get distance of nearest point
+        distances = np.square(distances)
         distances_norm = distances / np.sum(distances)
 
     # Apply the standard k-means algorithm, initialized with these means.
@@ -61,7 +64,6 @@ def mapper(key, value):
     nearestCenters = np.array(nearestCenters)
     for c, center in enumerate(centers):
         vectors_indices = np.where(nearestCenters == c)[0]
-        # print(vectors_indices)
         # print("center {} has {} 'nearest' vectors".format(c, len(vectors_indices)))
         if len(vectors_indices) > 0:
             vectors_by_centers[c] = np.take(value, vectors_indices, axis=0)
@@ -82,6 +84,22 @@ def reducer(key, values):
             new_centers.append(new_center)
 
     print("Centers: {}".format(len(new_centers)))
+
+    # merge nearest centers
+    while len(new_centers) > 200:
+        distances = np.full((len(new_centers)), np.inf)
+        # TODO can be optimized if not chosen by random, rather by smallest distances (but much slower)
+        z = np.random.choice(len(new_centers))
+        for j in range(len(new_centers)):
+            if j == z:
+                continue
+            distances[j] = measureDistance(new_centers[z], new_centers[j])
+
+        ind = np.argmin(distances)
+        new_center = np.mean([new_centers[z], new_centers[ind]], axis=0)
+
+        new_centers = np.delete(new_centers, [z, ind], axis=0)
+        new_centers = np.append(new_centers, [new_center], axis=0)
 
     # Output: 200 vectors representing the selected centers
     #         each being 250 floats
